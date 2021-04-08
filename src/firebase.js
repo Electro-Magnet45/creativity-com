094c3b20ac4561d24c35cdaeb542248d8777baa3
TheAswin <75930721+TheAswin@users.noreply.github.com>
2021-04-08 11:03:59 +0530

    Tenth Commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAUQG8gvrs0q6Dr1rzfCtxnK0baLD6bSa4",
-  authDomain: "creativity-com.firebaseapp.com",
-  projectId: "creativity-com",
-  storageBucket: "creativity-com.appspot.com",
-  messagingSenderId: "1026259966778",
-  appId: "1:1026259966778:web:1ca7ec1b7320c5e9bbce03",
+  apiKey: "AIzaSyBz3beNE88gymLP4IZh4JWRdoZ2HCTnQJU",
+  authDomain: "creativity-com-9fc94.firebaseapp.com",
+  projectId: "creativity-com-9fc94",
+  storageBucket: "creativity-com-9fc94.appspot.com",
+  messagingSenderId: "508445377050",
+  appId: "1:508445377050:web:f05090c69f13b5692ac7a3",
 };
 
 if (!firebase.apps.length) {
